ad1b0eb562a8ad167ea8cf4aa6f720949120c406
Mustafa Naser <sof9816@gmail.com>
2024-02-18 16:02:19 +0300

    ✏️ Change: auth logic

--- lib/misc/client/api_client.dart
+++ lib/misc/client/api_client.dart
@@ -4,26 +4,34 @@ import 'package:miswag_auth/misc/client/interceptors/debug_interceptor.dart';
 import 'package:miswag_auth/models/auth_options.dart';
 
 class ApiClient {
+  final AuthOptions options;
   final AuthOptions authOptions;
   final bool debug;
   Map<String, Object> authentication;
-
   Dio client;
   Dio authClient;
-
-  ApiClient(this.authOptions, {this.debug = false})
-      : assert(authOptions != null) {
+  ApiClient(this.options, this.authOptions, {this.debug = false})
+      : assert(authOptions != null, options != null) {
     final BaseOptions options = BaseOptions(
-        baseUrl: authOptions.baseUrl,
-        connectTimeout: authOptions.connectingTimeout,
-        receiveTimeout: authOptions.receivingTimeout,
+        baseUrl: this.options.baseUrl,
+        connectTimeout: this.options.connectingTimeout,
+        receiveTimeout: this.options.receivingTimeout,
         headers: {
           "Content-Type": "application/json",
           "Accept": "application/json"
         });
-    authClient = Dio(options);
+    final BaseOptions authOptions = BaseOptions(
+        baseUrl: this.authOptions.baseUrl,
+        connectTimeout: this.authOptions.connectingTimeout,
+        receiveTimeout: this.authOptions.receivingTimeout,
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        });
+    authClient = Dio(authOptions);
     client = Dio(options);
     client.interceptors.add(AuthenticationInterceptors(client: this));
+    authClient.interceptors.add(AuthenticationInterceptors(client: this));
     if (debug) {
       client.interceptors.add(DebugInterceptor());
     }
